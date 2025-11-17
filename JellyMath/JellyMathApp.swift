2f81063716d6ecd10e8cfa3e3532856ef9ccec67
Chao Chen <cherrycc93@gmail.com>
2025-11-17 15:12:04 -0500

    Initial Commit

---
+++ JellyMath/JellyMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  JellyMathApp.swift
+//  JellyMath
+//
+//  Created by Chao Chen on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct JellyMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
